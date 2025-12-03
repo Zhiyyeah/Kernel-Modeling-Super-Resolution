0cdce9f4d0cba007d58e7789ae166918a4cf5314
Zhiyye_Mac <zhiyyeah@gmail.com>
2025-12-04 00:13:28 +0800

    删除不相关的文件

--- test_code/utils.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def to_np(tensor):
-    tensor = tensor.detach().cpu().numpy()
-    tensor = np.squeeze(tensor)
-    if tensor.ndim == 3:
-        tensor = np.rollaxis(tensor, 0, 3)
-    return tensor
-
